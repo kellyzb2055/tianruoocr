59e98d609bc29cf6abe5072c5ac8c5b61547608b
topkill <fake@not.com>
2025-12-23 17:13:38 +0800

    ``` 发现一个bug：无论是识别结果窗口还是翻译结果窗口，窗口文本中如果含有有特定emoji时，比如❤️，如果使用的字体是Times New Roman，导致所有中文字符都变成方块，换成微软雅黑或宋体，中文字符恢复正常了，但是emoji渲染还是方块(微软雅黑)或者空白(宋体)，尝试升级RichEdit 内核以修复此bug，没修复成功，以后再修复吧，升级RichEdit 内核也先保留 ```

--- RichTextBoxEx.cs
+++ RichTextBoxEx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 using TrOCR.Helper;
 
 namespace TrOCR
@@ -25,9 +26,36 @@ namespace TrOCR
 
 		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
 		private static extern IntPtr LoadLibrary(string path);
+        // ====================【升级 RichEdit 内核】开始 ====================
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                // 获取基类的参数
+                CreateParams cp = base.CreateParams;
+                try
+                {
+                    // 尝试加载 Windows 自带的高级 RichEdit 内核 (msftedit.dll)
+                    // 该文件在 XP SP1 及以上系统均存在
+                    if (LoadLibrary("msftedit.dll") != IntPtr.Zero)
+                    {
+                        // 强行将类名修改为 RichEdit50W
+                        // 这将启用高级排版功能（包括彩色 Emoji 支持和更好的字体回退机制）
+                        cp.ClassName = "RichEdit50W";
+                    }
+                }
+                catch
+                {
+					// 如果加载失败（极少见），什么都不做，使用默认内核防止崩溃
+					CommonHelper.ShowHelpMsg("RichEdit加载内核失败,使用默认内核",10000);
+                }
+                return cp;
+            }
+        }
+        // ====================【升级 RichEdit 内核】结束 ====================
 
 
-		[Bindable(true)]
+        [Bindable(true)]
 		[RefreshProperties(RefreshProperties.All)]
 		[SettingsBindable(true)]
 		[DefaultValue(false)]
